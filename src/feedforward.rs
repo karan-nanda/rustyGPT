e63e3788e760cbf14b85ed313825829235073c84
karan_nanda <knanda66@gmail.com>
2025-09-17 22:32:14 -0600

    finished feedforward!

--- src/feedforward.rs
+++ src/feedforward.rs
@@ -33,6 +33,96 @@ impl FeedForward {
         let std_w1 = (2.0 / embedding_dim as f32).sqrt();
         let normal_w1 = Normal::new(0.0, std_w1).unwrap();
 
+        //Init for w2 : std = sqrt(2 / fan_in)
+        let std_w2= (2.0 / hidden_dim as f32).sqrt();
+        let normal_w2 = Normal::new(0.0, std_w2).unwrap();        
         
+        FeedForward {
+            w1 : Array2::from_shape_fn((embedding_dim, hidden_dim), |_| normal_w1.sample(&mut rng)),
+            b1 : Array2::zeros((1, hidden_dim)), //Bias init to 0
+            w2: Array2::from_shape_fn((hidden_dim, embedding_dim), |_| normal_w2.sample(&mut rng)),
+            b2: Array2::zeros((1, embedding_dim)), // Bias init to 0
+            input: None,
+            hidden_pre_activation: None,
+            hidden_post_activation: None,
+            optimizer_w1: Adam::new((embedding_dim, hidden_dim)),
+            optimizer_b1: Adam::new((1, hidden_dim)),
+            optimizer_w2: Adam::new((hidden_dim, embedding_dim)),
+            optimizer_b2: Adam::new((1, embedding_dim)),
+        }
     }
 }
+
+impl Layer for FeedForward {
+    fn layer_type (&self) -> &str {
+        "FeedForward"
+    }
+
+
+    fn backward (&mut self, grads: &Array2<f32>, lr: f32) -> Array2<f32> {
+        //unwrap the cached values
+        let input = self.input.as_ref().expect("forward must be run first!!");
+        let hidden_pre_activation = self.hidden_pre_activation.as_ref().unwrap();
+        let hidden_post_activation = self.hidden_post_activation.as_ref().unwrap();
+
+
+        //Compute the gradients for w2 and b2
+
+        let grad_w2 = hidden_post_activation.t().dot(grads);
+        let grad_b2 = grads.sum_axis(Axis(0)).insert_axis(Axis(0)); // shape: [1, embedding_dim]
+        
+
+        //Gradients for hidden_post_activation
+        let grad_hidden_post_activation = grads.dot(&self.w2.t());
+
+        //Gradient through ReLU
+        let relu_grad = hidden_pre_activation.mapv(|x| if x > 0.0 { 1.0 } else { 0.0 });
+        let grad_hidden_pre_activation = grad_hidden_post_activation * relu_grad;
+
+        //Gradient for w1 and b1
+        let grad_w1 = input.t().dot(&grad_hidden_pre_activation);
+        let grad_b1 = grad_hidden_pre_activation.sum_axis(Axis(0)).insert_axis(Axis(0)); // shape : [1, hidden_dim]
+
+
+        // Gradient for input (after feedforward computation)
+        let grad_input_feedforward = grad_hidden_pre_activation.dot(&self.w1.t());
+
+        //Add the gradients from residual connections
+        // Forward pass : output = w2(ReLU(w1* input + b1)) + b2 + input
+        // Backward pass : grad_input = grad_feedforward + grad_residual
+
+        let grad_input = grad_input_feedforward + grads ; 
+
+
+        // Update parameters via Adam optim
+        self.optimizer_w1.step(&mut self.w1, &grad_w1, lr);
+        self.optimizer_b1.step(&mut self.b1, &grad_b1, lr);
+        self.optimizer_w2.step(&mut self.w2, &grad_w2, lr);
+        self.optimizer_b2.step(&mut self.b2, &grad_b2, lr);
+
+
+        grad_input
+    }
+
+
+
+    fn forward(&mut self, input : &Array2<f32>) -> Array2<f32> {
+
+        let hidden_pre_activation = input.dot(&self.w1) + &self.b1;
+        let hidden_post_activation = hidden_pre_activation.mapv(|x| x.max(0.0)); // ReLU 
+        let output = hidden_post_activation.dot(&self.w2) + &self.b2;
+
+
+        //cached values
+
+        self.input = Some(input.clone());
+        self.hidden_pre_activation = Some(hidden_pre_activation);
+        self.hidden_post_activation = Some(hidden_post_activation);
+
+
+
+        output + input //residual connection (No need for a layerNorm)
+    }  
+}
+
+
